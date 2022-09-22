c7e4644be75cc9a9997ef826721540702106e25c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-09-22 17:55:07 +0900

    feat: add delete and update task methods using action sheet and alert controller

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -10,9 +10,6 @@ export class FolderPage implements OnInit {
   public folder: string;
   title = 'タスク登録';
 
-  /** 画面に表示するタスク一覧。 */
-  tasks: { name: string }[] = [];
-
   /** 入力値を受け付けて双方向バインディングする変数。 */
   task: string;
 
@@ -26,25 +23,16 @@ export class FolderPage implements OnInit {
   }
 
   /**
-   * ページ読み込み時のライフサイクルメソッド。
-   * ページを読み込む際に localStorage から永続化しているデータを取得する。
-   */
-  ionViewWillEnter(): void {
-    if (this.tasksLocalStorageKey in localStorage) {
-      this.tasks = JSON.parse(localStorage.tasks);
-    }
-  }
-
-  /**
    * 現在入力されている task の値を用いて、タスクを追加する。
    * localStorage.tasks に JSON.stringify() で文字列化したタスク一覧を保存する。
    * 追加後は、task の入力値を空に戻す。
    */
   addTask(): void {
-    this.tasks.push({ name: this.task });
-    // TODO: 下記の 2 つの違いを知りたい
-    // localStorage.tasks = JSON.stringify(this.tasks);
-    localStorage.setItem(this.tasksLocalStorageKey, JSON.stringify(this.tasks));
+    const tasks: { name: string }[] = JSON.parse(
+      localStorage.getItem(this.tasksLocalStorageKey)
+    );
+    tasks.push({ name: this.task });
+    localStorage.setItem(this.tasksLocalStorageKey, JSON.stringify(tasks));
     this.task = '';
   }
 }
--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-list',
@@ -18,7 +18,10 @@ export class TaskListPage implements OnInit {
   /** タスク一覧を LocalStorage に保存する際のキー名。 */
   readonly tasksLocalStorageKey = 'tasks';
 
-  constructor() {}
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController
+  ) {}
 
   /** Angular のライフサイクルで、ページを生成したときに一度だけ実行される。 */
   ngOnInit(): void {}
@@ -31,5 +34,77 @@ export class TaskListPage implements OnInit {
     if (this.tasksLocalStorageKey in localStorage) {
       this.tasks = JSON.parse(localStorage.tasks);
     }
+  }
+
+  /**
+   * @ionic/angular の ActionSheet を表示して
+   * タスクの変更アクションに関する選択肢を表示し、選択された内容を実行する。
+   *  */
+  async changeTask(index: number): Promise<void> {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.tasks.splice(index, 1);
+            localStorage.setItem(
+              this.tasksLocalStorageKey,
+              JSON.stringify(this.tasks)
+            );
+          },
+        },
+        {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            this.renameTask(index);
+          },
+        },
+        {
+          text: '閉じる',
+          role: 'キャンセル',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked.');
+          },
+        },
+      ],
+    });
+    actionSheet.present();
+  }
+
+  /** Prompt タイプアラートを表示して、
+   * 指定した番号のタスクの名前を変更する。 */
+  async renameTask(index: number): Promise<void> {
+    const prompt = await this.alertController.create({
+      header: '変更後のタスク',
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'タスク',
+          value: this.tasks[index].name,
+        },
+      ],
+      buttons: [
+        {
+          text: '閉じる',
+        },
+        {
+          text: '保存',
+          handler: (data) => {
+            // TODO: data.task がややこしい気がする
+            this.tasks[index] = { name: data.task };
+            localStorage.setItem(
+              this.tasksLocalStorageKey,
+              JSON.stringify(this.tasks)
+            );
+          },
+        },
+      ],
+    });
+    prompt.present();
   }
 }
